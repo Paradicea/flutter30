b33b80bb1f2cf548ace26a1fa583e9b6f7da1025
Paradicea <paradicea.ad@gmail.com>
2021-11-12 11:41:28 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int cars=500;
+  final String name="audi";
+  static const double pi=3.14;
+  final bool isACar=true;
+  final num temp=30.5;
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(title: Text("CATALOG APP"),),
+        body:Center(
+          child:Container(
+            child: Text("flutter yo"),
+            ),
+            ),drawer: Drawer(child: Center(child: Text("hello sir"),),
+            ),
+            ),
+    );
+   
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,26 @@
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({ Key? key }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home:Material(
-//         child:Container(
-//           child:Center(
-//             child: Text('hello world'),
-//             ),
-//           ),
-//       ),
-      
-//     );
-//   }
-// }
-
+import 'home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final int cars=500;
+  final String name="audi";
+  static const double pi=3.14;
+  final bool isACar=true;
+  final num temp=30.5;
+  
   const MyApp({ Key? key }) : super(key: key);
+ 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Material(
-        child:Container(
-          child:Center(child: Text('Hello world'),)
-        ),
-      ),
+      home:HomePage(),
+      
     );
   }
 }
+
